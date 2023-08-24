14f62ce32aa9d75ddb1dc30dee2e399b3b84cfda
Heisenberg <omaradelawad20013@gmail.com>
2023-08-24 13:48:34 +0300

    add proper auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 import RootLayout from './pages/RootLayout';
 import ErrorPage from './pages/ErrorPage';
@@ -7,19 +9,34 @@ import SingUp from './pages/SingUp';
 import LogIn from './pages/LogIn';
 import ForgotPassword from './pages/ForgotPassword';
 import Bookmarks from './pages/Bookmarks';
+import Loading from './components/Loading';
 
 const App = () => {
-  const isAuthenticated = false;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true)
+      }
+      setIsLoading(false);
+    });
+  }, [auth])
+
+  if (isLoading) return <Loading />
 
   return (
     <>
       <Routes>
+        <Route path='*' element={<ErrorPage />} />
         <Route path='/sign-up' element={!isAuthenticated ? <SingUp /> : <Navigate to='/' />} />
         <Route path='/log-in' element={!isAuthenticated ? <LogIn /> : <Navigate to='/' />} />
         <Route path='/forgot-password' element={!isAuthenticated ? <ForgotPassword /> : <Navigate to='/' />} />
         <Route path='/' element={<RootLayout />}>
           <Route path='/bookmarks' element={isAuthenticated ? <Bookmarks /> : <Navigate to='/' />} />
-          <Route path='*' element={<ErrorPage />} />
         </Route>
       </Routes>
     </>
