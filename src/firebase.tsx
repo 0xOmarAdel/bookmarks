424ac6894d3eaeef8e6f73665c605727123f40cd
Heisenberg <omaradelawad20013@gmail.com>
2023-08-24 11:06:55 +0300

    initialie firebase

---
+++ src/firebase.tsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWJ32n1pYl0y6H6EssFgJpiV1MNVxvL-M",
+  authDomain: "bookmarks-react-firebase.firebaseapp.com",
+  projectId: "bookmarks-react-firebase",
+  storageBucket: "bookmarks-react-firebase.appspot.com",
+  messagingSenderId: "631184849852",
+  appId: "1:631184849852:web:0974b4b0f8f7d1743c09b9"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+export const db = getFirestore();
