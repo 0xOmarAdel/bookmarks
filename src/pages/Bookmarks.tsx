8c73f62faaa5cf9d13ea812e26eeee6e72799033
Heisenberg <omaradelawad20013@gmail.com>
2023-08-25 16:25:46 +0300

    add error handling

--- src/pages/Bookmarks.tsx
+++ src/pages/Bookmarks.tsx
@@ -1,6 +1,31 @@
-const Bookmarks = () => {
+import BookmarkCategories from "../components/BookmarkCategories";
+import useGetFirestoreData from '../hooks/useGetFirestoreData';
+import Loading from "../components/Loading";
+import NewBookmarkForm from '../components/NewBookmarkForm';
+import { useEffect } from "react";
+
+const Bookmarks: React.FC = () => {
+  const {
+    data: categoriesData,
+    isLoading: categoriesDataLoading,
+    error: categoriesDataError,
+    reFetchData: reFetchCategoriesData
+  } = useGetFirestoreData('categories', null, null, 'title', 'asc', null);
+
+  useEffect(() => {
+    if (categoriesDataError && !categoriesDataLoading) {
+      console.log('error')
+    }
+  }, [categoriesDataError, categoriesDataLoading])
+  
+
+  if (categoriesDataLoading) return <Loading />;
+
   return (
-    <div>Bookmarks</div>
+    <>
+      <NewBookmarkForm categories={categoriesData} reFetchCategories={reFetchCategoriesData} />
+      <BookmarkCategories categories={categoriesData} />
+    </>
   );
 };
 
