cebc6e3411fae4e910dc44f5c04be674c7475f8e
Heisenberg <omaradelawad20013@gmail.com>
2023-08-25 16:16:39 +0300

    update icons and ts fix

--- src/components/BookmarkCategoryIcon.tsx
+++ src/components/BookmarkCategoryIcon.tsx
@@ -1,31 +1,35 @@
 import { BiBook, BiBookmark } from "react-icons/bi";
 import { TiDocumentText } from "react-icons/ti";
 import { CgGames } from "react-icons/cg";
-import { FaLanguage, FaDev } from "react-icons/fa";
+import { FaGraduationCap, FaRegMoneyBillAlt, FaLanguage, FaDev } from "react-icons/fa";
 import { GiPirateFlag } from "react-icons/gi";
 import { FiShoppingCart } from "react-icons/fi";
 import { BsPeople } from "react-icons/bs";
 
-const BookmarkCategoryIcon = ({ categoryTitle }) => {
-  switch (categoryTitle) {
+type Props = {
+  categoryTitle: string;
+}
+
+const BookmarkCategoryIcon : React.FC<Props> = (props) => {
+  switch (props.categoryTitle) {
     case 'books':
       return  <BiBook className='text-2xl' />;
     case 'courses':
-        return  <BsPeople className='text-2xl' />;
+      return  <FaGraduationCap className='text-2xl' />;
     case 'docs':
       return  <TiDocumentText className='text-2xl' />;
     case 'freelancing':
-        return  <BsPeople className='text-2xl' />;
+      return  <FaRegMoneyBillAlt className='text-2xl' />;
     case 'gaming':
       return  <CgGames className='text-2xl' />;
     case 'languages':
-        return  <FaLanguage className='text-2xl' />;
+      return  <FaLanguage className='text-2xl' />;
     case 'piracy':
-        return  <GiPirateFlag className='text-2xl' />;
+      return  <GiPirateFlag className='text-2xl' />;
     case 'programming':
       return  <FaDev className='text-2xl' />;
     case 'shopping':
-        return  <FiShoppingCart className='text-2xl' />;
+      return  <FiShoppingCart className='text-2xl' />;
     case 'social media':
       return  <BsPeople className='text-2xl' />;
     default:
