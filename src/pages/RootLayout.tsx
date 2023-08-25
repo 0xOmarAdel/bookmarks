fefe1807192d2b0cf2c22dac706bc84cbc7dbfa5
Heisenberg <omaradelawad20013@gmail.com>
2023-08-25 10:35:50 +0300

    responsiveness fixes

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -5,7 +5,7 @@ const RootLayout = () => {
   return (
     <div className='flex flex-row gap-16 min-h-screen'>
       <Sidebar />
-      <div className='grow my-16 pr-16'>
+      <div className='grow my-16 mx-8 sm:ml-16 md:ml-0 sm:mr-16'>
         <Outlet />
       </div>
     </div>
