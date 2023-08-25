dd8890e6cc0769f42ae4cefb05bc080b05510eca
Heisenberg <omaradelawad20013@gmail.com>
2023-08-25 09:40:00 +0300

    disable input suggestions

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -8,6 +8,7 @@ const Input = ({ type, id, placeholder, value, onChange, icon: Icon }) => {
         value={value}
         onChange={(e) => onChange(e.target.value)}
         className='w-full border border-gray-300 rounded py-1 pl-10 pr-2 outline-none'
+        autoComplete='off'
       />
       <div className='absolute top-1/2 -translate-y-1/2 p-2 border border-gray-300 bg-gray-100 text-primaryRed'>
         <Icon />
