2e483a91ce0669208ea676bbcc1acc814fba63c4
Heisenberg <omaradelawad20013@gmail.com>
2023-08-25 16:26:57 +0300

    update profile page ui

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,126 @@
-const Profile = () => {
+import { useEffect, useState } from "react";
+import Button from "../ui/Button";
+import useGetFirestoreData from "../hooks/useGetFirestoreData";
+import { getAuth, updateEmail, updatePassword, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
+import Loading from "../components/Loading";
+import { db } from "../firebase";
+import Card from "../ui/Card";
+import Input from "../ui/Input";
+import { AiOutlineUser, AiOutlineMail } from "react-icons/ai";
+import { VscKey } from "react-icons/vsc";
+
+const Profile: React.FC = () => {
+  const auth = getAuth();
+  const {
+    data: userData,
+    isLoading: userDataLoading,
+    error: userDataError,
+    reFetchData: reFetchUserData
+  } = useGetFirestoreData('users', auth.currentUser!.uid);
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [currentPassword1, setCurrentPassword1] = useState('');
+  const [currentPassword2, setCurrentPassword2] = useState('');
+  const [newPassword1, setNewPassword1] = useState('');
+  const [newPassword2, setNewPassword2] = useState('');
+  
+  useEffect(() => {
+    if (userData) {
+      setFirstName(userData.firstName);
+      setLastName(userData.lastName);
+      setEmail(userData.email);
+    }
+  }, [userData])
+
+  useEffect(() => {
+    if (userDataError && !userDataLoading) {
+      console.log('An error occurred!');
+    }
+  }, [userDataError, userDataLoading])
+  
+  
+  if (userDataLoading) {
+    return <Loading />;
+  }
+
+  const submitHandler = async (event: React.FormEvent) => {
+    try {
+      event.preventDefault();
+
+      await updateProfile(auth.currentUser!, {
+        displayName: firstName
+      });
+
+      await updateEmail(auth.currentUser!, email)
+
+      const docRef = doc(db, 'users', auth.currentUser!.uid);
+      await updateDoc(docRef, {
+        firstName,
+        lastName,
+        email,
+      });
+
+      reFetchUserData();
+      console.log('Updated your profile successfully!')
+    } catch (error) {
+      console.log(error)
+      console.log('An error occurred!')
+    }
+  }
+
+  const changePasswordHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (newPassword1 !== newPassword2) {
+      console.log('Both passwords need to match!')
+    } else if (newPassword1.length < 6) {
+      console.log('New password should be at least 6 characters!')
+    } else {
+      updatePassword(auth.currentUser!, newPassword1).then(() => {
+        console.log('Changed your password successfully!')
+      }).catch((error) => {
+        console.log(error)
+        console.log('An error occurred!')
+      });
+    }
+  };
+
   return (
-    <div>Profile</div>
+    <div className='flex flex-col lg:flex-row gap-12'>
+      <Card title='Account Info' className='w-full'>
+        <form onSubmit={submitHandler} className='grid xl:grid-cols-2 gap-6'>
+          <div className='w-full'>
+            <Input id='first-name' type='text' placeholder='First Name' value={firstName} onChange={(newValue) => setFirstName(newValue)} icon={AiOutlineUser} />
+          </div>            
+          <div className='w-full'>
+            <Input id='last-name' type='text' placeholder='Last Name' value={lastName} onChange={(newValue) => setLastName(newValue)} icon={AiOutlineUser} />
+          </div>
+          <div className='w-full'>
+            <Input id='email' type='text' placeholder='Last Name' value={email} onChange={(newValue) => setEmail(newValue)} icon={AiOutlineMail} />
+          </div>            
+          <div className='w-full'>
+            <Input id='current-password1' type='password' placeholder='Current Password' value={currentPassword1} onChange={(newValue) => setCurrentPassword1(newValue)} icon={VscKey} />
+          </div>
+          <Button text='Save Changes' className='mx-auto xl:mx-0' />
+        </form>
+      </Card>
+      <Card title='Change Password' className='w-full'>
+        <form onSubmit={changePasswordHandler} className='grid xl:grid-cols-2 gap-6'>
+          <div className='w-full'>
+            <Input id='current-password2' type='password' placeholder='Current Password' value={currentPassword2} onChange={(newValue) => setCurrentPassword2(newValue)} icon={VscKey} />
+          </div>            
+          <div className='w-full'>
+            <Input id='new-password1' type='password' placeholder='New Password' value={newPassword1} onChange={(newValue) => setNewPassword1(newValue)} icon={VscKey} />
+          </div>
+          <div className='w-full'>
+            <Input id='new-password2' type='password' placeholder='Repeat New Password' value={newPassword2} onChange={(newValue) => setNewPassword2(newValue)} icon={VscKey} />
+          </div>
+          <Button text='Save Changes' className='mx-auto xl:mx-0' />
+        </form>
+      </Card>
+    </div>
   );
 };
 
