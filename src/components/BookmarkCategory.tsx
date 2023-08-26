32b48e4a9692e58b7ddf4192198b009d1f57805e
Heisenberg <omaradelawad20013@gmail.com>
2023-08-26 13:19:22 +0300

    change category scrollbar appearance

--- src/components/BookmarkCategory.tsx
+++ src/components/BookmarkCategory.tsx
@@ -13,18 +13,18 @@ type Props = {
 const BookmarkCategory: React.FC<Props> = (props) => {
   return (
     <Card className='!w-full pb-10 md:bp-0!text-left'>
+      <div className='flex flex-row items-center gap-3 mb-5 text-primaryRed font-semibold'>
+        <BookmarkCategoryIcon categoryTitle={props.title} />
+        <h3 className='text-2xl capitalize whitespace-nowrap'>{props.title}</h3>
+      </div>
       <ScrollableDiv className="max-h-80">
-          <div className='flex flex-row items-center gap-3 mb-5 text-primaryRed font-semibold'>
-            <BookmarkCategoryIcon categoryTitle={props.title} />
-            <h3 className='text-2xl capitalize whitespace-nowrap'>{props.title}</h3>
-          </div>
-          <ul className='flex flex-col gap-3 text-xl capitalize'>
-            {
-              props.bookmarks.map(bookmark =>
-                <BookmarkCategoryItem key={bookmark.id} bookmark={bookmark} reFetchCategories={props.reFetchCategories} />
-              )
-            }
-          </ul>
+        <ul className='flex flex-col gap-3 text-xl capitalize'>
+          {
+            props.bookmarks.map(bookmark =>
+              <BookmarkCategoryItem key={bookmark.id} bookmark={bookmark} reFetchCategories={props.reFetchCategories} />
+            )
+          }
+        </ul>
       </ScrollableDiv>
     </Card>
   );
