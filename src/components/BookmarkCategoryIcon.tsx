0cdc9558600b4fd46316342cc04dd13e70f5bd03
Heisenberg <omaradelawad20013@gmail.com>
2023-08-27 11:49:03 +0300

    update categories icons

--- src/components/BookmarkCategoryIcon.tsx
+++ src/components/BookmarkCategoryIcon.tsx
@@ -1,37 +1,48 @@
-import { BiBook, BiBookmark } from "react-icons/bi";
-import { TiDocumentText } from "react-icons/ti";
-import { CgGames } from "react-icons/cg";
+import { BiDumbbell, BiBook, BiBookmark } from "react-icons/bi";
+import { SiProbot } from "react-icons/si";
+import { AiOutlineFileText, AiOutlineFolderOpen } from "react-icons/ai";
 import { FaGraduationCap, FaRegMoneyBillAlt, FaLanguage, FaDev } from "react-icons/fa";
 import { GiPirateFlag } from "react-icons/gi";
 import { FiShoppingCart } from "react-icons/fi";
-import { BsPeople } from "react-icons/bs";
+import { VscKey } from "react-icons/vsc";
+import { BsController, BsPeople, BsGear } from "react-icons/bs";
 
 type Props = {
   categoryTitle: string;
 }
 
 const BookmarkCategoryIcon : React.FC<Props> = (props) => {
-  switch (props.categoryTitle) {
+  switch (props.categoryTitle.toLocaleLowerCase()) {
+    case 'ai':
+      return  <SiProbot className='text-2xl' />;
     case 'books':
       return  <BiBook className='text-2xl' />;
+    case 'challenges':
+      return  <BiDumbbell className='text-2xl' />;
     case 'courses':
       return  <FaGraduationCap className='text-2xl' />;
     case 'docs':
-      return  <TiDocumentText className='text-2xl' />;
+      return  <AiOutlineFileText className='text-2xl' />;
     case 'freelancing':
       return  <FaRegMoneyBillAlt className='text-2xl' />;
     case 'gaming':
-      return  <CgGames className='text-2xl' />;
+      return  <BsController className='text-2xl' />;
     case 'languages':
       return  <FaLanguage className='text-2xl' />;
+    case 'libraries':
+      return  <AiOutlineFolderOpen className='text-2xl' />;
     case 'piracy':
       return  <GiPirateFlag className='text-2xl' />;
     case 'programming':
       return  <FaDev className='text-2xl' />;
     case 'shopping':
       return  <FiShoppingCart className='text-2xl' />;
+    case 'shortcuts':
+      return  <VscKey className='text-2xl' />;
     case 'social media':
       return  <BsPeople className='text-2xl' />;
+    case 'tools':
+      return  <BsGear className='text-2xl' />;
     default:
       return  <BiBookmark className='text-2xl' />;
   }
