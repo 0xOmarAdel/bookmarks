95e99624551e0566f937fce263214a1411801424
Heisenberg <omaradelawad20013@gmail.com>
2023-10-05 22:35:28 +0300

    fixed pages y axis margin on small screens

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -3,9 +3,9 @@ import Sidebar from '../components/Sidebar'
 
 const RootLayout: React.FC = () => {
   return (
-    <div className='flex flex-row  min-h-screen'>
+    <div className='flex flex-row min-h-screen'>
       <Sidebar />
-      <div className='grow my-16 mx-8 sm:ml-16 md:ml-0 sm:mr-16 md:pl-72'>
+      <div className='grow mt-8 mb-24 sm:mt-16 sm:mb-32 md:my-16 mx-8 sm:ml-16 md:ml-0 sm:mr-16 md:pl-72'>
         <Outlet />
       </div>
     </div>
